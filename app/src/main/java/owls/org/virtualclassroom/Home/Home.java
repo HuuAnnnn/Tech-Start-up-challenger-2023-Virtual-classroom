04254d783900fd2762317314d5dd20adc194a149
NguyenHuuAn <huuan04112002@gmail.com>
2023-07-16 22:58:37 +0700

    feat: add setting page

--- app/src/main/java/owls/org/virtualclassroom/Home/Home.java
+++ app/src/main/java/owls/org/virtualclassroom/Home/Home.java
@@ -1,16 +1,27 @@
 package owls.org.virtualclassroom.Home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import owls.org.virtualclassroom.Profile.Profile;
 import owls.org.virtualclassroom.R;
 
 public class Home extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        changeFragment(new Profile());
+    }
+
+    private void changeFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = manager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.commit();
     }
 }
