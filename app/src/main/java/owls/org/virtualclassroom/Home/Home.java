2010109c46e56378fac3d5c140a0e74a0a233189
NguyenHuuAn <huuan04112002@gmail.com>
2023-07-17 00:03:44 +0700

    feat: change feature screen on click the bottom navigation

--- app/src/main/java/owls/org/virtualclassroom/Home/Home.java
+++ app/src/main/java/owls/org/virtualclassroom/Home/Home.java
@@ -1,21 +1,44 @@
 package owls.org.virtualclassroom.Home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import owls.org.virtualclassroom.Profile.Profile;
 import owls.org.virtualclassroom.R;
 
 public class Home extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        changeFragment(new Profile());
+        bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.settings:
+                        changeFragment(new Profile());
+                        break;
+                    case R.id.explore:
+                        break;
+                    case R.id.my_course:
+                        break;
+                }
+
+                return true;
+            }
+        });
     }
 
     private void changeFragment(Fragment fragment) {
