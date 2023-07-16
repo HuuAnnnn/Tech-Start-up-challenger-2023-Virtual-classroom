c6ee26d1c1407bda9310d54d01bcf6fac247335f
NguyenHuuAn <huuan04112002@gmail.com>
2023-07-16 15:52:17 +0700

    feat: add font settings and basic splash screen

---
+++ app/src/main/java/owls/org/virtualclassroom/SplashScreen/SplashScreen.java
@@ -0,0 +1,16 @@
+package owls.org.virtualclassroom.SplashScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import owls.org.virtualclassroom.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
