6cb734aa0288eedb7b09b3413dcbde85db81411e
NguyenHuuAn <huuan04112002@gmail.com>
2023-07-20 13:28:15 +0700

    feat: learning screen

---
+++ app/src/main/java/owls/org/virtualclassroom/Learn/Learn.java
@@ -0,0 +1,16 @@
+package owls.org.virtualclassroom.Learn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import owls.org.virtualclassroom.R;
+
+public class Learn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn);
+    }
+}
